17ad2866007ab32c4ee2ab95dd50b33000d5be84
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-23 21:35:36 +0000

    fixed average

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 "use strict";
 /**
- * This function calculates the average
+ * This function calculates the average and range
  */
 function calculate () {
   // input
@@ -14,24 +14,12 @@ function calculate () {
   let test2 = parseFloat(document.getElementById('test2').value);
 
   // process
-  let average = test1 * test2 /2;
-
-  // output
-  document.getElementById('average').innerHTML = 'Average is: ' + average + '%.
-}
-
-/**
- * This function calculates the range
- */
-function calculate () {
-  // input
-  let test1 = parseFloat(document.getElementById('test1').value);
-  let test2 = parseFloat(document.getElementById('test2').value);
-
-  // process
+  let total = test1 + test2;
+  let average = total /2;
   let range = test1 - test2;
 
   // output
-  document.getElementById('range').innerHTML = 'Range is: ' + Math.abs(range) + '.'
+  document.getElementById('average').innerHTML = 'Average is: ' + average.toFixed(2) + '%.'
+  // output
+  document.getElementById('range').innerHTML = 'Range is: ' + Math.abs(range).toFixed(2) + '.'
 }
-
